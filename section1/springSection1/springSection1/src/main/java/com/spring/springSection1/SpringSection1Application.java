5820cc8db84b2c9c7e62325172de7fe7d514c4e2
D1_63027_Aishwarya <aishwaryabhagwat7@gmail.com>
2025-05-16 16:10:17 +0530

    main changes

---
+++ section1/springSection1/springSection1/src/main/java/com/spring/springSection1/SpringSection1Application.java
@@ -0,0 +1,13 @@
+package com.spring.springSection1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSection1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSection1Application.class, args);
+	}
+
+}
---
+++ section1/springSection1/springSection1/src/test/java/com/spring/springSection1/SpringSection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springSection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSection1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
